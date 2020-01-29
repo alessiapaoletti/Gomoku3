00bf4752147ddcc334a173d24344e45b3247c2b4
edonadini <43779349+edonadini@users.noreply.github.com>
2020-01-29 12:12:31 +0100

    test player

--- src/test/java/Model/PlayerTest.java
+++
@@ -1,9 +0,0 @@
-package Model;
-imp
-import static org.junit.Assert.*;
-
-public class PlayerTest {
-    Player b=new Player("Ele",PieceColor.BLACK);
-    Player w=new Player("Nora",PieceColor.WHITE);
-
-}
