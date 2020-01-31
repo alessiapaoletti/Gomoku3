0ba290bf6fbc4d1cfae7f577959734e0254ba7d2
alessiapaoletti <paoletti.alessia@gmail.com>
2020-01-31 12:34:09 +0100

    update Players

---
+++ src/main/java/Model/BlackPlayer.java
@@ -0,0 +1,8 @@
+package Model;
+
+public class BlackPlayer extends  Player {
+
+    public BlackPlayer(String name) {
+        super(name, PieceColor.BLACK);
+    }
+}
---
+++ src/main/java/Model/WhitePlayer.java
@@ -0,0 +1,8 @@
+package Model;
+
+public class WhitePlayer extends Player {
+
+    public WhitePlayer(String name) {
+        super(name, PieceColor.WHITE);
+    }
+}
